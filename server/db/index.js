3eb3b01ca9d91c83d59a7fc2a4ed1d507ec2536f
darkavengerk <darkavengerk@gmail.com>
2018-03-21 12:48:45 +0900

    Evironments setup

--- server/db/index.js
+++ server/db/index.js
@@ -1,22 +1,8 @@
 import { DB_TYPE } from '../../config/env';
-import { DB_TYPES } from '../../config/dbTypes';
 
 let dbConfig = null;
 
-/* use inline requires for conditional loading */
-switch (DB_TYPE) {
-  case DB_TYPES.MONGO:
-    dbConfig = require('./mongo').default;
-    break;
-  case DB_TYPES.POSTGRES:
-    dbConfig = require('./postgres').default;
-    break;
-  case DB_TYPES.NONE:
-    dbConfig = require('./none').default;
-    break;
-  default:
-    throw new Error(`No database type '${DB_TYPE}' found`);
-}
+dbConfig = require('./mongo').default;
 
 export const connect = dbConfig.connect;
 export const controllers = dbConfig.controllers;
