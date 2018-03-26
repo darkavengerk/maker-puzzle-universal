8738e01cfc36931cfa4703f128bc9c4a3911cad9
darkavengerk <darkavengerk@gmail.com>
2018-03-26 12:02:39 +0900

    check

--- config/app.js
+++ config/app.js
@@ -5,7 +5,7 @@ export const isProduction = ENV === 'production';
 export const isDebug = ENV === 'development';
 export const isClient = typeof window !== 'undefined';
 
-export const apiEndpoint = isDebug ? 'http://localhost:3000' : 'http://localhost';
+export const apiEndpoint = isDebug ? 'http://localhost:3000' : 'http://ec2-13-124-27-145.ap-northeast-2.compute.amazonaws.com';
 // Replace with 'UA-########-#' or similar to enable tracking
 export const trackingID = null;
 
