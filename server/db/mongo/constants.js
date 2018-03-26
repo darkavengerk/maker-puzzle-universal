4727399eb474b418d775a6fc543a8c649c683b22
darkavengerk <darkavengerk@gmail.com>
2018-03-26 17:18:55 +0900

    db setting

--- server/db/mongo/constants.js
+++ server/db/mongo/constants.js
@@ -1,4 +1,8 @@
-export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://localhost/ReactWebpackNode';
+import {db_uri} from '../../../config/secrets'
+
+console.log(db_uri);
+
+export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || db_uri;
 
 export default {
   db
