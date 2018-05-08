521ad548963dae2d145e0df5e04076cdea694fbe
Jeonghyeon Kim <darkavengerk@gmail.com>
2018-05-09 03:46:40 +0900

    content editing

--- app/components/MakerProfile.jsx
+++ app/components/MakerProfile.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import classNames from 'classnames/bind';
 import FlexibleImage from '../components/FlexibleImage';
 import Border from '../components/SingleLine';
+import ImageUploader from '../components/ImageUploader';
 import styles from '../css/components/maker-profile';
 
 import ContentEditable from 'react-contenteditable'
@@ -10,95 +12,128 @@ import jsxToString from 'jsx-to-string';
 
 const cx = classNames.bind(styles);
 
-const MakerProfile = ({ maker }) => {
-
-  let stats = (
-    <span className={cx('stats-area', 'flex-row')}>
-      <span className={cx('maker-stats', 'flex-col')}>
-        <span className={cx('figure')}>
-          {maker.portfolios.length}
-        </span>
-        <span className={cx('keyword')}>
-          Portfolio
-        </span>
-      </span>
-      <span className={cx('maker-stats', 'flex-col')}>
-        <span className={cx('figure')}>
-          421
-        </span>
-        <span className={cx('keyword')}>
-          Follower
-        </span>
-      </span>
-      <span className={cx('maker-stats', 'flex-col')}>
-        <span className={cx('figure')}>
-          421
-        </span>
-        <span className={cx('keyword')}>
-          Following
-        </span>
-      </span>
-    </span>);
-
-  let splitLetters = word => {
-    return word.split('').map((letter,i) => {
-      return (<span key={i}>{letter}</span>);
-    })
+class MakerProfile extends Component {
+  constructor(props) {
+    super(props);
   }
 
-  const handleChange = ev => console.log(ev);
+  render() {
+    const { maker } = this.props;
 
-  let features = maker.features.map(feature => {
-    return (
-      <div className={cx('feature-item')} key={feature.title}>
-        <span className={cx('feature-title')}>
-          {splitLetters(feature.title)}
+    let stats = (
+      <span className={cx('stats-area', 'flex-row')}>
+        <span className={cx('maker-stats', 'flex-col')}>
+          <span className={cx('figure')}>
+            {maker.portfolios.length}
+          </span>
+          <span className={cx('keyword')}>
+            Portfolio
+          </span>
         </span>
-        <ContentEditable 
-          html={ jsxToString(<span className={cx('feature')}>
-            {feature.content}
-            </span>)
-          }
-          onChange={handleChange}
-        />
-        
+        <span className={cx('maker-stats', 'flex-col')}>
+          <span className={cx('figure')}>
+            421
+          </span>
+          <span className={cx('keyword')}>
+            Follower
+          </span>
+        </span>
+        <span className={cx('maker-stats', 'flex-col')}>
+          <span className={cx('figure')}>
+            421
+          </span>
+          <span className={cx('keyword')}>
+            Following
+          </span>
+        </span>
+      </span>);
+
+    let splitLetters = word => {
+      return word.split('').map((letter,i) => {
+        return (<span key={i}>{letter}</span>);
+      })
+    }
+
+    const handleChange = evt => {
+      if (evt.key === 'Enter') {
+          evt.preventDefault();
+      }
+      return false;
+    }
+
+    const handleChange2 = evt => {
+      let current = evt.target;
+      let parent = current.parentElement.parentElement;
+      let children = parent.childNodes;
+      current.innerText;
+    }
+
+    let features = maker.features.map(feature => {
+      return (
+        <div className={cx('feature-item')} key={feature.title}>
+          <span className={cx('feature-title')}>
+            {splitLetters(feature.title)}
+          </span>
+          <ContentEditable 
+            tagName="span"
+            className={cx('feature')}
+            html={ feature.content }
+            onKeyPress={handleChange}
+            placeholder="Say anything"
+          />
+          
+        </div>
+      );
+    });
+
+    let featureArea = (
+      <div className={cx('feature-area')}>
+          {features}
       </div>
     );
-  });
 
-  let featureArea = (
-    <div className={cx('feature-area')}>
-        {features}
-    </div>
-  );
-
-  return (
-    <div className={cx('main-section')}>
-      <span className={cx('flex-row')}>
-        <FlexibleImage src="/images/default_profile.jpg" x={144} y={144} />
-        <span className={cx('user-info')}>
-          <span className={cx('name')}>
-            {maker.profile.name}
-          </span>
-          <Border width={181} thickness={2} color={'#dadada'} />
-          {stats}
-          <span className={cx('follow-button')} role="button">
-            FOLLOW
+    return (
+      <div className={cx('main-section')}>
+        <span className={cx('flex-row')}>
+          <FlexibleImage src="/images/default_profile.jpg" x={144} y={144} />
+          <span className={cx('user-info')}>
+            <span className={cx('name')}>
+              {maker.profile.name}
+            </span>
+            <Border width={181} thickness={2} color={'#dadada'} />
+            {stats}
+            <span className={cx('follow-button')} role="button">
+              FOLLOW
+            </span>
           </span>
         </span>
-      </span>
-      <div>
-        {featureArea}
-        <div className={cx('about-maker')}>
-          {maker.about.split('\n').map((sen,i) => (<p key={i}>{sen}</p>))}
+        <div>
+          {featureArea}
+          <ContentEditable 
+            className={cx('about-maker')}
+            html={ jsxToString(
+              <div>
+                {maker.about.split('\n').map((sen,i) => (<p key={i}>{sen}</p>))}
+              </div>
+            ) } 
+            onKeyPress={handleChange2}
+          />
+          
         </div>
+        <ImageUploader name="ImageUploader" />
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 MakerProfile.propTypes = {
   maker: PropTypes.object.isRequired
 };
 
-export default MakerProfile;
+function mapStateToProps(state) {
+  return {
+    maker: state.maker.maker
+  };
+}
+
+export default connect(mapStateToProps, {})(MakerProfile);
