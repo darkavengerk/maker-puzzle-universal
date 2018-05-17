8b2e06e7e7399d426088303b349e32b89937ccf4
Jeonghyeon Kim <darkavengerk@gmail.com>
2018-05-17 10:14:59 +0900

    elastic ip

--- config/app.js
+++ config/app.js
@@ -5,7 +5,7 @@ export const isProduction = ENV === 'production';
 export const isDebug = ENV === 'development';
 export const isClient = typeof window !== 'undefined';
 
-export const apiEndpoint = isDebug ? 'http://localhost:3000' : 'http://ec2-13-124-27-145.ap-northeast-2.compute.amazonaws.com';
+export const apiEndpoint = isDebug ? 'http://localhost:3000' : 'http://13.209.54.193';
 // Replace with 'UA-########-#' or similar to enable tracking
 export const trackingID = null;
 
