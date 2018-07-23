7e625cddc95f8bc458e5b08e4c75ba9e67129741
Jeonghyeon Kim <darkavengerk@gmail.com>
2018-07-23 12:00:48 +0900

    bug fix

--- app/components/Switch.jsx
+++ app/components/Switch.jsx
@@ -3,24 +3,19 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import Switch from "react-switch";
 
-import FlexibleImage from '../components/FlexibleImage';
-import ImageUploader from '../components/ImageUploader';
-import Scatter from '../components/Scatter';
-import styles from '../css/components/add-portfolio';
-
-const cx = classNames.bind(styles);
-
 const OnOffSwitch = ({ onChange, checked, name, ...props }) => {
 
   return (
-    <Switch 
-        onChange={onChange}
-        checked={checked || false}
-        id={name}
-        onColor="#ee5400"
-        uncheckedIcon={false}
-        checkedIcon={false}
-      />
+    <span style={{'text-align':'left'}}>
+      <Switch 
+          onChange={onChange}
+          checked={checked || false}
+          id={name}
+          onColor="#ee5400"
+          uncheckedIcon={false}
+          checkedIcon={false}
+        />
+    </span>
   );
 }
 
