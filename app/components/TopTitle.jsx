8f98f8944ea2456e32dca517115db41cbbae48d2
Jeonghyeon Kim <darkavengerk@gmail.com>
2018-09-04 12:03:33 +0900

    title bar float

--- app/components/TopTitle.jsx
+++ app/components/TopTitle.jsx
@@ -1,28 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import classNames from 'classnames/bind';
 import styles from '../css/components/top-title';
 import ProfileImage from '../components/FlexibleImage';
+import SingleLine from '../components/SingleLine';
 
 const cx = classNames.bind(styles);
 
-const TitleSection = ({ title, thumbnailURL, to, props}) => {
+class TitleSection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {scrollY: 0}
+    this.handleScroll = this.handleScroll.bind(this);
+  }
 
-  const profileImage = thumbnailURL? 
-        <ProfileImage src={thumbnailURL} className={cx('profile-image')} x={30} y={30}/>
-         : null;
+  handleScroll(evt) {
+    this.setState({scrollY: window.scrollY});
+  }
 
-  return (
-      <Link to={to} className={cx('main-section')}>
-        {profileImage}
-        <span className={cx('title')}>
-          {title}
-        </span>
-      </Link>
-  );
-};
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  render() {
+    const { title, thumbnailURL, to, props} = this.props
+
+    const profileImage = thumbnailURL? 
+          <ProfileImage src={thumbnailURL} className={cx('profile-image')} x={30} y={30}/>
+           : null;
+
+    const titleBar = <Link to={to} className={cx('main-section')}>
+          {profileImage}
+          <span className={cx('title')}>
+            {title}
+          </span>
+        </Link>;
+
+    const showFloat = this.state.scrollY > 150;
+
+    return ( 
+      <div>
+        <div className={cx('main-section')}>
+          {titleBar}
+        </div>
+        {
+          showFloat? 
+          <div className={cx('float-section', showFloat)}>
+            {titleBar}
+            <SingleLine width={'100%'} color={'#dddddd'} thickness={2} />
+          </div> : null
+        }
+        
+      </div>
+    );
+  };
+}
 
 TitleSection.propTypes = {
   title: PropTypes.string
