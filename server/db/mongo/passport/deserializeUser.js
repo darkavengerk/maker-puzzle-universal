483d35bfdcf812c4b42f5509826a004f3302a1b0
Jeonghyeon Kim <darkavengerk@gmail.com>
2019-01-21 16:20:01 +0900

    session map

--- server/db/mongo/passport/deserializeUser.js
+++ server/db/mongo/passport/deserializeUser.js
@@ -1,7 +1,18 @@
 import User from '../models/user';
 
+const sessionMap = {};
+
 export default (id, done) => {
+  const now = new Date().getTime();
+  if(sessionMap[id]) {
+    const time = sessionMap[id].time;
+    if(now - time < 5000) {
+      sessionMap[id].time = now;
+      return done(null, sessionMap[id].user);
+    }
+  }
   User.findById(id, (err, user) => {
+    sessionMap[id] = {user, time: new Date()};
     done(err, user);
   });
 };
