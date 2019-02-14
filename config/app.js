7ff855b24d256200b8c245ca791c0db2a4f5daa4
Jeonghyeon Kim <darkavengerk@gmail.com>
2019-02-14 13:56:00 +0900

    env setting

--- config/app.js
+++ config/app.js
@@ -7,7 +7,7 @@ export const isProduction = ENV === 'production';
 export const isDebug = ENV === 'development';
 export const isClient = typeof window !== 'undefined';
 
-export const apiEndpoint = isDebug ? 'https://localhost' : 'https://www.maker-puzzle.com';
+export const apiEndpoint = isDebug ? 'http://localhost:3000' : 'https://www.maker-puzzle.com';
 // Replace with 'UA-########-#' or similar to enable tracking
 export const trackingID = 'UA-128508172-1';
 
