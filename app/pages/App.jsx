49983af1c20a0efdb30eeee732bcc0085a9d53f6
Jeonghyeon Kim <darkavengerk@gmail.com>
2019-04-05 19:01:24 +0900

    browser bug fix

--- app/pages/App.jsx
+++ app/pages/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Page from '../pages/Page';
 import AppContainer from '../containers/App';
+import BrowserWarning from '../components/BrowserWarning';
 import { title, meta, link } from './assets';
 
 const App = props => (
   <Page title={title} meta={meta} link={link}>
+    <BrowserWarning />
     <AppContainer {...props} />
   </Page>
 );
